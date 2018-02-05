3904157941435380cbe85fa77e95914b57594075
YuraBoyco <boyco-yura@yandex.ru>
2018-02-05 15:40:32 +0300

    Add anchors

--- js/main.js
+++ js/main.js
@@ -3,5 +3,21 @@ $(document).ready(function() {
         $(".menu-list").slideToggle();
         $(".menu-list").removeClass('hide');
     });
+
+    $("#menu-list li").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор блока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		//alert(id);
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
 });
 
